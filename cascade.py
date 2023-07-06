bbedda79e6250b48d6206f9c210f63ab4592f85d
jameshawkes <James.Hawkes@ecmwf.int>
2023-07-07 00:02:51 +0100

    drafting of graph definition and basic execution structure

--- cascade.py
+++ cascade.py
@@ -1,64 +1,82 @@
 import randomname
 import numpy as np
 import cupy as cp
-import copy
+import ucp
 
+################
+
+
+class DataDef:
+    def __init__(self, dtype=None, size=None, name=None):
+        self.name = randomname.get_name()
+
+
+class TaskDef:
+    def __init__(self, function, inputs, outputs, name=None):
+        self.name = function.__name__
+        self.inputs = inputs if isinstance(inputs, list) else [inputs]
+        self.outputs = outputs if isinstance(outputs, list) else [outputs]
+
+
+################
 
 
 class Task:
+    def __init__(self, taskdef):
+        self.taskdef = taskdef
+        self.host = ucp.get_address(ifname="eth0")
+        self.downstream = []  # data references
+        self.upstream = []  # data references
 
-    def __init__(self, graph, definition):
-        self.function = definition[0]
-        self.dependencies = []
-        self.input = []
-        self.output = []
-        self.dependents = []
-        self.status = "NOT_STARTED"
-
-        for i in range(1, len(definition)):
-            self.dependencies.append(graph.create_or_get_task(definition[i]))
-
-        for dependency in self.dependencies:
-            dependency.dependents.append(self)
-
-    def get_inputs(self):
-        # receive inputs from each dependency task
-    
     def execute(self):
-        self.function(*self.input)
+        # call recv on all upstream data references
+        # run the function
+        # notify data available and call send on all downstream data references (dependencies can start)
+        # notify and shutdown once complete (resources now free)
+        pass
 
-    def notify(self):
-        # tell the manager that we have completed and output is ready
 
-    def put_outputs(self):
-        # send outputs to each dependent task
+class Data:
+    def __init__(self, datadef):
+        self.datadef = datadef
+        self.downstream = []  # task references
+        self.upstream = None  # task reference
 
-    def run(self):
-        self.get_inputs()
-        self.execute()
-        self.notify()
-        self.put_outputs()
+    def notify_manager(self):
+        pass
+
+    def send(self):
+        # create a listener
+        # send
+        pass
+
+    def recv(self):
+        # create an endpoint
+        # receive
+        pass
+
+
+################
 
 
 class Graph:
-
-    def __init__(self, from_dict):
-        self.computations = copy.deepcopy(from_dict)
+    def __init__(self):
+        self.taskdefs = {}
         self.tasks = {}
 
-    def compute(self, key):
+    def add(self, task):
+        for o in task.outputs:
+            self.taskdefs[o] = task
 
-        self.create_or_get_task(key)
+    def compute(self, datadef):
+        assert datadef in self.taskdefs
 
-        root_tasks = []
-        for t in self.tasks:
-            if len(t.dependencies) == 0:
-                root_tasks.append(t)
+        # Create tasks by picking from the library of taskdefs
+        # Start from the final task and work upstream
 
+        task = self.taskdefs[datadef]
 
-
-    def create_or_get_task(self, key):
-        return self.computations.get(key, Task(self, self.computations[key]))
+        # when data is notified as available, check dependents of that data
 
 
 def read():
@@ -75,7 +93,6 @@ def max(a):
 
 
 def main():
-
     # data objects, contains a name (generated) as well as information about the data size
 
     # build a graph which says which tasks produce which data objects and which consume it
@@ -85,12 +102,17 @@ def main():
 
     # call get() with a set [] of output data objects to create
 
-    G = Graph({
-        "a": (read),
-        "m": (max, "a")
-        })
+    a = DataDef()
+    m = DataDef()
 
-    G.compute("m")
+    t1 = TaskDef(read, None, a)
+    t2 = TaskDef(max, a, m)
+
+    G = Graph()
+    G.add(t1)
+    G.add(t2)
+
+    G.compute(m)
 
 
 main()
