bbf619e951381665e6d61dfdbb2a022210d7e048
Tiago Quintino <tiago.quintino@ecmwf.int>
2023-07-13 11:43:55 +0100

    WIP

--- experiments/jax-playground.py
+++ experiments/jax-playground.py
@@ -1,11 +1,10 @@
-from jax import jit
-from jax import numpy as jnp
+import jax
 
 def slow_f(x):
   return x * x + x * 2.0 # element-wise ops see a large benefit from fusion
 
-x = jnp.ones((5000, 5000))
-fast_f = jit(slow_f)
+x = jax.numpy.ones((5000, 5000))
+fast_f = jax.jit(slow_f)
 
 fast_f(x) 
 slow_f(x) 
