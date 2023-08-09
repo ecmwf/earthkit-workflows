789d977d904e5b42b38b3389867a06c961caa0b9
majh <James.Hawkes@ecmwf.int>
2023-08-09 15:47:21 +0100

    first attempt at function cascade API

--- experiments/fluent.py
+++ experiments/fluent.py
@@ -1,6 +1,8 @@
 import randomname
 from ppgraph import Node, Graph
 from typing import List
+from ppgraph import pyvis
+from itertools import product
 
 def _mean(x):
     count = 0
@@ -9,7 +11,45 @@ def _mean(x):
         sum += m
         count += 1
     return sum/count
-        
+
+def _sum(x):
+    sum = 0
+    for m in x:
+        sum += m
+    return sum
+
+def _read(x):
+    # actually read the data one by one
+    pass
+
+def spectral_transform(x):
+    return x
+
+def _expand_request(req):
+    metadata = {}
+    for r in req.split(","):
+        lhs, rhs = r.split("=")
+        if "to" in rhs:
+            rhs += "/by/1" # add default values
+            frm,_,to,_,by,*_ = rhs.split("/")
+            metadata[lhs] = [i for i in range(int(frm), int(to), int(by))]
+        else:
+            metadata[lhs] = [rhs]
+
+    prod = list(product(*metadata.values()))
+    product_with_keys = [{k: v for k, v in zip(metadata.keys(), values)} for values in prod]
+    return product_with_keys
+    
+def _combine_dicts(dict_list):
+    combined = defaultdict(list)
+    for d in dict_list:
+        for key, value in d.items():
+            if isinstance(value, list):
+                combined[key].extend(value)
+            else:
+                combined[key].append(value)
+    return dict(combined)
+
 
 class Action:
     def __init__(self, func, root=None, count=1):
@@ -20,8 +60,12 @@ class Action:
 
     # -- Generators
 
-    def mean(self):
-        self.next = MultiToSingleAction(_mean, self.root)
+    def mean(self, key=""):
+        self.next = MultiToSingleAction(_mean, self.root )
+        return self.next
+    
+    def sum(self):
+        self.next = MultiToSingleAction(_mean, self.root )
         return self.next
     
     def then(self, func):
@@ -32,19 +76,44 @@ class Action:
         self.next = MultiToMultiAction(func, self.root)
         return self.next
     
-    def read(self, size):
-        self.next = SingleToMultiAction(lambda x: [x for i in range(size)], self.root, size)
+    def groupby(self, key):
+        self.next = GroupingAction(key, self.root)
+        return self.next
+    
+    def constant(self, value):
+        self.next = SingleToSingleAction(lambda x: value, self.root)
+        return self.next
+    
+    def repeat(self, count):
+        self.next = MultiToMultiAction(lambda x: x, self.root, self.count * count)
+        return self.next
+    
+    def write(self):
+        self.next = SingleToSingleAction(lambda x: print(x), self.root)
+        return self.next
+    
+    def threshold(self, threshold):
+        self.next = MultiToMultiAction(lambda x: 1 if x > threshold else 0, self.root)
         return self.next
     
     # -- Graph Generation
     
     def graph(self) -> List[Node]:
-        return self.root._graph(None)
+        return Graph(self.root._graph(None))
 
     def _graph(self, previous: List[Node]) -> List[Node]:
         # root node does nothing, just returns the next node
         return self.next._graph(None)
-        
+
+class Cascade(Action):
+    def __init__(self):
+        super().__init__(None)
+
+    def read(self, request):
+        fields = _expand_request(request)
+        self.next = SingleToMultiAction(lambda x: _read(x), self.root, len(fields))
+        return self.next
+
 
 class SingleToSingleAction(Action):
     
@@ -70,7 +139,7 @@ class MultiToMultiAction(Action):
         for p in previous:
             node = Node(randomname.generate(), payload=self.func, input=p)
             nodes.append(node)
-        
+
         if self.next is None:
             return nodes
         return self.next._graph(nodes)
@@ -88,6 +157,7 @@ class SingleToMultiAction(Action):
                 assert len(previous) == 1
                 nodes[c].inputs[randomname.generate()] = previous[0].get_output()
         
+
         if self.next is None:
             return nodes
 
@@ -107,20 +177,100 @@ class MultiToSingleAction(Action):
         
         return self.next._graph([node])
 
+class GroupingAction(Action):
 
-root = Action(None)
+    def __init__(self, key, root):
+        super().__init__(None, root)
+        if key == "step":
+            self.count = 50 # hacky until we track metadata we can actually group on
 
-windows = [[0, 20], [10, 30], [20, 40], [30, 50], [40, 60], [50, 70], [60, 80]]
+    def _graph(self, previous):
+        
+        assert previous is not None
+
+        nodes = []
+        n = 0
+        for p in previous:
+            if n % self.count == 0:
+                node = Node(randomname.generate(), payload=self.func)
+                nodes.append(node)
+            else:
+                node = nodes[-1]
+            node.inputs[str(n % self.count)] = p.get_output()
+            n += 1
+        
+        if self.next is None:
+            return nodes
+        return self.next._graph(nodes)
+
+# -------------------------------------------------------------------------------------
+
+Cascade().read("stream=efhs,levtype=pl,level=850,step=0-240/by/12").foreach(spectral_transform).mean().write()
+
+# Cascade().constant(1).repeat(50).sum().write()
 
 
-g = Graph([])
 
-for window in windows:
-    root = Action(None)
-    window_graph = root.read(size=20).foreach(lambda x: x+1).mean().graph()
+# -------------------------------------------------------------------------------------
 
-# Cascade::read().foreach().mean().then()
-# Cascade::merge().mean().then().graph()
+# windows = [[0, 20], [10, 30], [20, 40], [30, 50], [40, 60], [50, 70], [60, 80]]
+window_ranges = [[120, 240], [168, 240]]
+window_cascades = []
+total_graph = Graph([])
+
+for window in window_ranges:
+
+    start = window[0]
+    end = window[1]
+
+    climatology = Cascade().read(f"stream=efhs,levtype=pl,level=850,step={start}-{end}")\
+                    .foreach(spectral_transform)
+    
+    t850 = Cascade().read(f"date=...,levtype=pl,level=850,number=1/to/50,step={start}/to/{end}/by/12")\
+                    .foreach(spectral_transform)\
+                    .groupby("step")\
+                    .foreach(lambda x: x - 2)\
+                    .mean("step")\
+                    .threshold("< -2")\
+                    .mean("number")\
+                    .write()
+    
+    # Join not yet supported
+    if False:
+        t850 = Cascade().read(f"date=...,levtype=pl,level=850,number=1/to/50,step={start}/to/{end}/by/12")\
+                        .foreach(spectral_transform)\
+                        .groupby("step")\
+                        .join(climatology)\
+                        .foreach(lambda x, y: x - y)\
+                        .mean("step")\
+                        .threshold("< -2")\
+                        .mean("number")\
+                        .write()
+    
+    if False:
+        t850 = Cascade().join(t850, climatology).foreach(lambda x, y: x - y).mean("step").threshold("< -2").mean("number").write()
+
+    window_cascades.append(t850)
+
+    # draw graph    
+    g = t850.graph()
+    pyvis.to_pyvis(g, notebook=True, cdn_resources="remote").show(f"graph_{window}.html")
+    total_graph += g
+
+if False:
+    # concatenate not yet supported
+    Cascade().concatenate(*total_graph).schedule()
+
+if False:
+    # some other examples of fluent queries
+    take10 = Cascade().orderby(lambda x: x.step).take(10)
+    other = Cascade().read()
+
+    # could implement forking (not yet supported)
+    take10.foreach(lambda x: x + 1).mean().then(lambda x: x + 1).write()
+    take10.mean().join(other).sum().write()
+
+
 
 # orderby
 # groupby
@@ -135,5 +285,15 @@ for window in windows:
 
 
 
-from ppgraph import pyvis
-pyvis.to_pyvis(g, notebook=True).show("test.html")
+
+pyvis.to_pyvis(g, notebook=True, cdn_resources="remote").show("index.html")
+
+
+# # -----------------------
+
+# Dask notes: 
+
+# cant go into nested functions
+# cant do foreach, we have to code it explicitly
+# have to wrap every function in a dask.delayed(func)(args) call
+# annotations using with:
