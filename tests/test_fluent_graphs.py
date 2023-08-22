7238dcc33e163ff9daf6041969fa4f6a0b806c42
Jenny Wong <mawj@ac6-102.bullx>
2023-08-22 09:42:18 +0000

    Add expected number of nodes to graph template tests

--- tests/test_fluent_graphs.py
+++ tests/test_fluent_graphs.py
@@ -1,6 +1,8 @@
 import pytest
 import os
 
+from ppgraph import pyvis
+
 from cascade.graph_config import Config
 from cascade.cascade import Cascade
 
@@ -8,15 +10,22 @@ ROOT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 
 @pytest.mark.parametrize(
-    "product, config",
+    "product, config, expected_num_nodes",
     [
-        ["prob", f"{ROOT_DIR}/templates/prob.yaml"],
-        ["anomaly_prob", f"{ROOT_DIR}/templates/t850.yaml"],
-        ["wind", f"{ROOT_DIR}/templates/wind.yaml"],
-        ["ensms", f"{ROOT_DIR}/templates/ensms.yaml"],
-        ["extreme", f"{ROOT_DIR}/templates/extreme.yaml"],
+        ["prob", f"{ROOT_DIR}/templates/prob.yaml", 252],
+        ["anomaly_prob", f"{ROOT_DIR}/templates/t850.yaml", 342],
+        ["wind", f"{ROOT_DIR}/templates/wind.yaml", 132],
+        ["ensms", f"{ROOT_DIR}/templates/ensms.yaml", 180],
+        ["extreme", f"{ROOT_DIR}/templates/extreme.yaml", 139],
     ],
 )
-def test_graph_construction(product, config):
+def test_graph_construction(product, config, expected_num_nodes):
     cfg = Config(config)
-    Cascade.graph(product, cfg)
+    graph = Cascade.graph(product, cfg)
+    if True:
+        pyvis_graph = pyvis.to_pyvis(graph, notebook=True, 
+                       cdn_resources="remote")
+        pyvis_graph.show(f"/etc/ecmwf/nfs/dh1_home_a/mawj/Documents/cascade/{product}_graph.html")
+    print([x for x in graph.nodes()])
+    assert len(pyvis_graph.nodes) == expected_num_nodes
+    #assert len([x for x in graph.nodes()]) == expected_num_nodes
