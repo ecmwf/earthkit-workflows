77c3d47fc6f5c28b05812e244bcfe2c7ce693831
Jenny Wong <mawj@ac6-101.bullx>
2023-09-13 13:05:19 +0000

    Add colours to input and output nodes

--- tests/test_fluent_graphs.py
+++ tests/test_fluent_graphs.py
@@ -1,12 +1,34 @@
 import pytest
 import os
+import functools
 
-from ppgraph import pyvis
+from ppgraph import pyvis, Sink
 
 from cascade.graph_config import Config
 from cascade.cascade import Cascade
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
+
+def node_info_ext(sinks, node):
+    info = pyvis.node_info(node)
+    if not node.inputs:
+        info["shape"] = "diamond"
+        info["color"] = "red"
+    elif node in sinks:
+        info["shape"] = "triangle"
+        info["color"] = "green"
+    if node.payload is not None:
+        t = []
+        if "title" in info:
+            t.append(info["title"])
+        func, *args = node.payload
+        t.append(f"Function: {func}")
+        if args:
+            t.append("Arguments:")
+            t.extend(f"- {arg!r}" for arg in args)
+        info["title"] = "\n".join(t)
+    return info
 
 
 @pytest.mark.parametrize(
@@ -28,6 +50,7 @@ def test_graph_construction(product, config, expected_num_nodes):
             notebook=True,
             cdn_resources="remote",
             height="1500px",
+            node_attrs=functools.partial(node_info_ext, graph.sinks),
             hierarchical_layout=False,
         )
         pyvis_graph.show(
