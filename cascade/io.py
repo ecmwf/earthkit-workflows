ea83499fdc448da60a7e617dc4a2572bf933fc51
Jenny Wong <mawj@ab6-205.bullx>
2023-11-22 15:03:16 +0000

    Fix mir cache and extraction from file

--- cascade/io.py
+++ cascade/io.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import importlib
 from os.path import join as pjoin
+import shutil
 
 from pproc.common.io import (
     target_from_location,
@@ -17,17 +18,15 @@ from pproc.clustereps.cluster import get_output_keys
 from pproc.common.io import split_location
 
 import mir
-from earthkit.data.sources.stream import StreamSource
 from earthkit.data import FieldList
+from earthkit.data.sources.stream import StreamSource
+from earthkit.data.sources.file import FileSource
 from earthkit.data.sources import Source, from_source
 from earthkit.data.sources.numpy_list import NumpyFieldList
 from earthkit.data.sources import register
 
 from .grib import basic_headers
-from .wrappers.mars import MarsRetrieverWithCache
 from .wrappers.metadata import GribBufferMetaData
-
-register("mars_with_cache", MarsRetrieverWithCache)
 
 
 def _source_from_location(loc, sources) -> tuple[str, list[dict]]:
@@ -41,12 +40,19 @@ def _source_from_location(loc, sources) -> tuple[str, list[dict]]:
     return type_, requests
 
 
-def mir_job(input: mir.MultiDimensionalGribFileInput, mir_options: dict) -> Source:
+def mir_job(
+    input: mir.MultiDimensionalGribFileInput, mir_options: dict, cache: str = None
+) -> Source:
     job = mir.Job(**mir_options)
     stream = BytesIO()
     job.execute(input, stream)
     stream.seek(0)
-    return StreamSource(stream, batch_size=0).mutate()
+    if cache is None:
+        return StreamSource(stream, batch_size=0).mutate()
+
+    with open(cache, "wb") as o, stream as i:
+        shutil.copyfileobj(i, o)
+    return FileSource(cache).mutate()
 
 
 def fdb_retrieve(request: dict, *, stream: bool = True) -> Source:
@@ -65,15 +71,23 @@ def fdb_retrieve(request: dict, *, stream: bool = True) -> Source:
 
 def mars_retrieve(request: dict) -> Source:
     mir_options = request.pop("interpolate", None)
-    ds = from_source("mars_with_cache", request)
+    cache = request.pop("cache", None)
+    cache_path = None if cache is None else cache.format_map(request)
+    ds = from_source("mars", request)
     if mir_options:
         size = len(request["param"]) if isinstance(request["param"], list) else 1
         inp = mir.MultiDimensionalGribFileInput(ds.path, size)
-        ds = mir_job(inp, mir_options)
+        ds = mir_job(inp, mir_options, cache_path)
     return ds
 
 
-def _transform_request(request: dict):
+def _transform_steps(steps, step_type: type = str):
+    if isinstance(steps, (int, str)):
+        steps = [steps]
+    return list(map(step_type, steps))
+
+
+def _transform_request(request: dict, step_type: type = str):
     try:
         paramId = int(request["param"])
         del request["param"]
@@ -83,13 +97,7 @@ def _transform_request(request: dict):
     request["date"] = int(request["date"])
     time = int(request["time"])
     request["time"] = time if time % 100 == 0 else time * 100
-    step = request["step"]
-    if isinstance(step, int):
-        request["step"] = str(step)
-    elif isinstance(step, (list, np.ndarray)):
-        request["step"] = list(map(str, step))
-    else:
-        assert isinstance(step, int)
+    request["step"] = _transform_steps(request["step"], step_type)
     return request
 
 
@@ -98,7 +106,14 @@ def file_retrieve(path: str, request: dict) -> Source:
     if mir_options:
         raise NotImplementedError()
     location = path.format_map(request)
-    ds = from_source("file", location).sel(_transform_request(request))
+    file_ds = from_source("file", location)
+    ds = file_ds.sel(_transform_request(request))
+    if len(ds) == 0:
+        try:
+            request["step"] = _transform_steps(request["step"], int)
+            ds = file_ds.sel(request)
+        except ValueError:
+            pass
     return ds
 
 
@@ -135,17 +150,13 @@ def retrieve_single_source(request: dict, **kwargs) -> NumpyFieldList:
             ret_sources = file_retrieve(path, req)
         else:
             raise NotImplementedError("Source {source} not supported.")
-    ret = None
-    for source in ret_sources:
-        field_list = FieldList.from_numpy(
-            xp.asarray([source.values]),
-            GribBufferMetaData(source.metadata()),
-        )
-        if ret is None:
-            ret = field_list
-        else:
-            ret += field_list
-    assert ret is not None, f"No data retrieved from {source} for request {request}"
+    assert (
+        len(ret_sources) > 0
+    ), f"No data retrieved from {source} for request {request}"
+    ret = FieldList.from_numpy(
+        xp.asarray(ret_sources.values),
+        list(map(GribBufferMetaData, ret_sources.metadata())),
+    )
     return ret
 
 
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import numpy as np
+import pytest
 
 from cascade.io import retrieve, write
 
@@ -28,3 +29,58 @@ def test_retrieve(tmpdir):
     fdb_request["source"] = "fdb"
     data2 = retrieve([request, fdb_request])
     assert np.all(data.values == data2.values)
+
+
+@pytest.mark.skip(reason="Hangs if run in test suite")
+def test_multiprocess(tmpdir):
+    import multiprocessing
+    import dill
+
+    dill.Pickler.dumps, dill.Pickler.loads = dill.dumps, dill.loads
+    multiprocessing.reduction.ForkingPickler = dill.Pickler
+    multiprocessing.reduction.dump = dill.dump
+    multiprocessing.queues._ForkingPickler = dill.Pickler
+    import concurrent.futures as fut
+
+    futures = []
+    base_request = request.copy()
+    with fut.ProcessPoolExecutor(max_workers=2) as executor:
+        for x in range(1, 3):
+            base_request["type"] = "pf"
+            base_request["number"] = x
+            futures.append(executor.submit(retrieve, base_request))
+
+    for future in fut.as_completed(futures):
+        data = future.result()
+        write(f"{tmpdir}/test.grib", data, {"step": 12})
+
+
+def test_mars_cache(tmpdir):
+    request = {
+        "class": "od",
+        "date": 20231116,
+        "domain": "g",
+        "expver": "0001",
+        "interpolate": {
+            "accuracy": "24",
+            "grid": "1.5/1.5",
+            "intgrid": "none",
+            "legendre-loader": "shmem",
+            "matrix-loader": "file-io",
+        },
+        "levelist": 500,
+        "levtype": "pl",
+        "param": 129,
+        "stream": "enfo",
+        "time": "1200",
+        "type": "es",
+        "source": "mars",
+        "step": [120, 132, 144, 156, 168],
+        "cache": f"{tmpdir}/es_z500_{{date}}{{time}}_{{step[0]}}",
+    }
+    mars_fl = retrieve(request)
+    request.pop("interpolate")
+    location = request.pop("cache")
+    request.update(source="fileset", location=location)
+    file_fl = retrieve(request)
+    assert np.all(mars_fl.values == file_fl.values)
