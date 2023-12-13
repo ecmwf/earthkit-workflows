0fa3bf87c9745dbf544e52f2437ac1d739f7cf4f
Jenny Wong <mawj@ac6-101.bullx>
2023-12-13 15:16:01 +0000

    Test execution output type

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -9,6 +9,7 @@ from cascade.fluent import source, Payload
 
 input = np.random.rand(2, 3)
 
+
 def graph(payloads):
     return (
         source(payloads, ["x", "y"])
@@ -19,21 +20,27 @@ def graph(payloads):
         .graph()
     )
 
-@pytest.mark.parametrize("payload", [
-    Payload(np.asarray, [input]), 
-    Payload(xr.DataArray, [input], {"dims": ["a", "b"]})
-])
-def test_graph_execution(payload):
+
+@pytest.mark.parametrize(
+    "payload, output_type",
+    [
+        [Payload(np.asarray, [input]), np.ndarray],
+        [Payload(xr.DataArray, [input], {"dims": ["a", "b"]}), xr.DataArray],
+    ],
+)
+def test_graph_execution(payload, output_type):
     payloads = np.empty((4, 5), dtype=object)
     payloads[:] = payload
     g = graph(payloads)
 
-    os.environ["DASK_LOGGING__DISTRIBUTED"]="debug"
+    os.environ["DASK_LOGGING__DISTRIBUTED"] = "debug"
     schedule = Schedule(g, None, {})
     executor = DaskExecutor(schedule)
     output = executor.execute(memory_limit="10GB", n_workers=1, threads_per_worker=1)
     assert len(output) == 3
     assert output[0].shape == (2,)
+    assert np.all([isinstance(x, output_type) for x in output])
+
 
 def test_graph_execution_jax():
     jax = pytest.importorskip("jax")
@@ -41,9 +48,10 @@ def test_graph_execution_jax():
     payloads[:] = Payload(jax.numpy.asarray, [input])
     g = graph(payloads)
 
-    os.environ["DASK_LOGGING__DISTRIBUTED"]="debug"
+    os.environ["DASK_LOGGING__DISTRIBUTED"] = "debug"
     schedule = Schedule(g, None, {})
     executor = DaskExecutor(schedule)
     output = executor.execute(memory_limit="10GB", n_workers=1, threads_per_worker=1)
     assert len(output) == 3
     assert output[0].shape == (2,)
+    assert np.all([isinstance(x, jax.Array) for x in output])
