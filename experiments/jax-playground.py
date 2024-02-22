e59db2e0e113b6a6f1805c4ba23a84e13053ea19
Jenny Wong <jenny.wong@ecmwf.int>
2024-02-22 14:03:27 +0000

    Remove experiments dir

--- experiments/jax-playground.py
+++
@@ -1,9 +0,0 @@
-import jax
-jax.config.update("jax_enable_x64", True) # use float64 -- jax by default uses float32
-
-def slow_f(x):
-  return x * x + x * 2.0 # element-wise ops see a large benefit from fusion
-
-x = jax.numpy.ones((5000, 5000))
-fast_f = jax.jit(slow_f)
-
