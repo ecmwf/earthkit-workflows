e4e2d81c1693185b8f777bf8eaff52959a6bdeb2
Jenny Wong <jenny.wong@ecmwf.int>
2024-02-22 11:45:57 +0000

    Tidy installation

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-import io
-import re
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-__version__ = re.search(
-    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-    io.open("cascade/version.py", encoding="utf_8_sig").read(),
-).group(1)
-
-
-setup(
-    name="cascade-python",
-    version=__version__,
-    description="cascade is a Python library for scheduling tasks on heterogeneous computing systems.",
-    long_description="""cascade is a Python library for scheduling tasks on heterogeneous computing systems.""",
-    url="https://github.com/ecmwf/cascade",
-    author="ECMWF",
-    author_email="",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=[
-        "randomname",
-        "numpy",
-        "xarray",
-        "networkx",
-        "dask",
-        "bokeh"
-    ]
-)
+setup()
