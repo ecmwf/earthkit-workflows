66a8bb75863ca86f41fd8715d5e845818b989deb
Vojta Tuma <vojta.tuma@gmail.com>
2024-10-07 16:49:39 +0200

    [controller] add simulation and scheduling
    [controller] added simulating executor
    [controller] add test job record builders
    [controller] implement multiple static schedulers
    [controller] dynamic scheduler skeleton
    [controller] dynamic scheduler fixes
    [controller] add scattering

--- src/cascade/controller/impl.py
+++ src/cascade/controller/impl.py
@@ -3,12 +3,15 @@ Implements the canonical cascade controller
 """
 
 # TODO major improvements
-# - scatter variables to the workers that will need them
+# - smarter scatter -- do beforehand, dictate by scheduler
+# - smarter purges -- per-worker, dictate by scheduler
 # - support for multiple outputs / generators per task
-# - control precisely which futures to launch already -- currently we just throw in first 2 in the queue per host
+# - control precisely which futures to launch already -- currently we just throw in first in the queue per host
 # - handle failed states, restarts, etc
 
 import logging
+from functools import reduce
+from typing import cast
 
 from cascade.controller.api import (
     ExecutableTaskInstance,
@@ -16,7 +19,10 @@ from cascade.controller.api import (
     PurgingPolicy,
     VariableWiring,
 )
-from cascade.low.core import JobInstance, Schedule, TaskInstance
+from cascade.low.core import JobExecutionRecord, JobInstance, Schedule, TaskInstance
+from cascade.low.func import maybe_head
+from cascade.low.scheduler.api import EnvironmentState, Scheduler
+from cascade.low.scheduler.dynamic import DynamicScheduler
 from cascade.low.views import dependants, param_source
 
 logger = logging.getLogger(__name__)
@@ -44,57 +50,113 @@ def _task2executable(
     )
 
 
+def is_dataset_needed(
+    schedule: Schedule,
+    dependants: dict[tuple[str, str], set[str]],
+    dataset: tuple[str, str],
+    purging_policy: PurgingPolicy,
+    environment_state: EnvironmentState,
+) -> bool:
+    if not purging_policy.eager or dataset in purging_policy.preserve:
+        return True
+    # TODO we recompute this from scratch because of the changing schedule. Wasteful, fix
+    remaining_tasks = reduce(
+        lambda acc, e: acc.union(e),
+        schedule.host_task_queues.values(),
+        schedule.unallocated,
+    ).union({e for _, e in environment_state.runningTaskAtHost})
+    # TODO we should make this more fine-grained -- now we either keep all copies, or discard all copies
+    return dependants[dataset].intersection(remaining_tasks) != set()
+
+
 def _submit(
     job: JobInstance,
     schedule: Schedule,
     executor: Executor,
+    execution_record: JobExecutionRecord,
     purging_policy: PurgingPolicy,
-) -> None:
+    environment_state: EnvironmentState,
+    dynamic_scheduler: Scheduler | None,
+) -> EnvironmentState:
     task_param_sources = param_source(job.edges)
     executable_tasks = {
         task: _task2executable(task, instance, job, task_param_sources[task])
         for task, instance in job.tasks.items()
     }
     id2task: dict[str, str] = {}
-
-    output_dependants: dict[tuple[str, str], set[str]] = {}
-    if purging_policy.eager:
-        output_dependants = {
-            k: v
-            for k, v in dependants(job.edges).items()
-            if v and k not in purging_policy.preserve
-        }
-    host_ongoing: dict[str, list[str]] = {
-        host: [] for host in schedule.host_task_queues.keys()
+    task2output: dict[str, tuple[str, str]] = {
+        task: (task, cast(str, maybe_head(instance.definition.output_schema)))
+        for task, instance in job.tasks.items()
     }
-    host_remaining = {host: tasks for host, tasks in schedule.host_task_queues.items()}
+    task_dependants = dependants(job.edges)
+    host_ongoing: dict[str, list[str]] = {
+        host: [] for host in executor.get_environment().hosts
+    }
+    host_remaining = {host: queue for host, queue in schedule.host_task_queues.items()}
 
     while True:
-        for host in schedule.host_task_queues.keys():
+        logger.debug(f"{host_ongoing=}")
+        logger.debug(f"{host_remaining=}")
+        for host in executor.get_environment().hosts.keys():
             while len(host_ongoing[host]) > 0 and executor.is_done(
                 host_ongoing[host][0]
             ):
                 completed = host_ongoing[host].pop(0)
                 logger.debug(f"finished {completed} on {host}")
-                for v in task_param_sources[id2task[completed]].values():
-                    output_dependants[v].remove(id2task[completed])
-                    if not output_dependants[v]:
+                environment_state = environment_state.finishTaskAt(
+                    host, id2task[completed]
+                )
+                environment_state = environment_state.computeDatasetAt(
+                    host, task2output[id2task[completed]]
+                )
+                for v in set(task_param_sources[id2task[completed]].values()):
+                    if not is_dataset_needed(
+                        schedule, task_dependants, v, purging_policy, environment_state
+                    ):
                         logger.debug(f"{v} not needed, purging")
-                        executor.purge(v[0], v[1])
-            while len(host_ongoing[host]) < 2 and len(host_remaining[host]) > 0:
+                        for other_host in environment_state.hosts_of_ds(v):
+                            executor.purge(v[0], v[1], {other_host})
+                            environment_state = environment_state.purgeDatasetAt(
+                                other_host, (v[0], v[1])
+                            )
+            while len(host_ongoing[host]) < 1 and len(host_remaining.get(host, [])) > 0:
                 nextTaskName = host_remaining[host].pop(0)
                 nextTask = executable_tasks[nextTaskName]
-                logger.debug(f"submitting {nextTask} on worker {host}")
+                logger.debug(f"submitting {nextTaskName} on worker {host}")
                 nextTaskId = executor.run_at(nextTask, host)
+                environment_state = environment_state.runTaskAt(host, nextTaskName)
                 host_ongoing[host].append(nextTaskId)
                 id2task[nextTaskId] = nextTaskName
+                reqs = {(e.sourceTask, e.sourceOutput) for e in nextTask.wirings}
+                if missing := (reqs - environment_state.ds_of_host(host)):
+                    # TODO this should happen *before* the submit, and be dictated by scheduler
+                    logger.debug(
+                        f"task {nextTaskName} on worker {host} is {missing =}! Scattering."
+                    )
+                    for e in missing:
+                        executor.scatter(e[0], e[1], {host})
+                        environment_state = environment_state.computeDatasetAt(
+                            host, (e[0], e[1])
+                        )
         ongoing = set(fut for futs in host_ongoing.values() for fut in futs)
         if len(ongoing) > 0:
-            logger.debug(f"awaiting on {len(ongoing)} futures")
+            logger.debug(f"awaiting on {len(ongoing)} futures {ongoing}")
             executor.wait_some(ongoing)
-        else:
-            logger.debug("nothing runnin, breaking")
+        if dynamic_scheduler is not None and (
+            not host_remaining.values()
+            or min(len(v) for v in host_remaining.values()) == 0
+        ):
+            logger.debug("idle worker, asking dynamic scheduler for more")
+            schedule = dynamic_scheduler.schedule(
+                job, executor.get_environment(), execution_record, environment_state
+            ).get_or_raise()
+            host_remaining = {
+                host: queue for host, queue in schedule.host_task_queues.items()
+            }
+        if len(environment_state.finished_tasks()) == len(job.tasks):
+            logger.debug("everything complete, breaking")
             break
+    return environment_state
 
 
 class CascadeController:
@@ -103,6 +165,20 @@ class CascadeController:
         job: JobInstance,
         schedule: Schedule,
         executor: Executor,
-        purging_policy: PurgingPolicy,
-    ) -> None:
-        return _submit(job, schedule, executor, purging_policy)
+        execution_record: JobExecutionRecord | None = None,
+        purging_policy: PurgingPolicy | None = None,
+        dynamic_scheduler: DynamicScheduler | None = None,
+    ) -> EnvironmentState:
+        if purging_policy is None:
+            purging_policy = PurgingPolicy()
+        if execution_record is None:
+            execution_record = JobExecutionRecord()
+        return _submit(
+            job,
+            schedule,
+            executor,
+            execution_record,
+            purging_policy,
+            EnvironmentState(),
+            dynamic_scheduler,
+        )
---
+++ src/cascade/low/scheduler/api.py
@@ -0,0 +1,111 @@
+import logging
+from dataclasses import dataclass, field, replace
+from itertools import chain
+from typing import Callable, Protocol, runtime_checkable
+
+from pyrsistent import PSet
+from pyrsistent import s as pset
+from typing_extensions import Self
+
+from cascade.low.core import Environment, JobExecutionRecord, JobInstance, Schedule
+from cascade.low.func import Either
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class EnvironmentState:
+    """Keeps track of what was computed already and where.
+    Used if we are doing a re-schedule (eg after task crashes or early finishes),
+    or by iterative scheduling algorithms"""
+
+    datasetAtHost: PSet[tuple[str, tuple[str, str]]] = field(default_factory=pset)
+    runningTaskAtHost: PSet[tuple[str, str]] = field(default_factory=pset)
+    finishedTaskAtHost: PSet[tuple[str, str]] = field(default_factory=pset)
+    # TODO add crash record
+    # TODO optimize the structures -- perhaps dict[host][ts|ds] is better
+
+    def finished_tasks(self) -> set[str]:
+        return {e for _, e in self.finishedTaskAtHost}
+
+    def started_tasks(self) -> set[str]:
+        return {e for _, e in chain(self.finishedTaskAtHost, self.runningTaskAtHost)}
+
+    def available_datasets(self) -> set[tuple[str, str]]:
+        return {e for _, e in self.datasetAtHost}
+
+    def hosts_of_ds(self, dataset: tuple[str, str]) -> set[str]:
+        return {h for h, d in self.datasetAtHost if d == dataset}
+
+    def ds_of_host(self, host: str) -> set[tuple[str, str]]:
+        return {e for h, e in self.datasetAtHost if h == host}
+
+    def ds_and_ts_of_host(self, host: str) -> tuple[set[tuple[str, str]], set[str]]:
+        """Datasets and running tasks"""
+        return (
+            {e for h, e in self.datasetAtHost if h == host},
+            {e for h, e in self.runningTaskAtHost if h == host},
+        )
+
+    def runTaskAt(self, host: str, task: str) -> Self:
+        logger.debug(f"running {task=} at {host=}")
+        return replace(
+            self,
+            runningTaskAtHost=self.runningTaskAtHost.add((host, task)),
+        )
+
+    def finishTaskAt(self, host: str, task: str) -> Self:
+        logger.debug(f"finishing {task=} at {host=}")
+        return replace(
+            self,
+            finishedTaskAtHost=self.finishedTaskAtHost.add((host, task)),
+            runningTaskAtHost=self.runningTaskAtHost.remove((host, task)),
+        )
+
+    def computeDatasetAt(self, host: str, dataset: tuple[str, str]) -> Self:
+        logger.debug(f"computed {dataset=} at {host=}")
+        return replace(
+            self,
+            datasetAtHost=self.datasetAtHost.add((host, dataset)),
+        )
+
+    def purgeDatasetAt(self, host: str, dataset: tuple[str, str]) -> Self:
+        logger.debug(f"purged {dataset=} at {host=}")
+        return replace(
+            self,
+            datasetAtHost=self.datasetAtHost.remove((host, dataset)),
+        )
+
+
+@runtime_checkable
+class Scheduler(Protocol):
+    def schedule(
+        self,
+        job_instance: JobInstance,
+        environment: Environment,
+        execution_record: JobExecutionRecord | None,
+        environment_state: EnvironmentState,
+    ) -> Either[Schedule, str]:
+        raise NotImplementedError
+
+
+class ClasslessScheduler:
+    def __init__(
+        self,
+        f: Callable[
+            [JobInstance, Environment, JobExecutionRecord | None, EnvironmentState],
+            Either[Schedule, str],
+        ],
+    ) -> None:
+        self.f = f
+
+    def schedule(
+        self,
+        job_instance: JobInstance,
+        environment: Environment,
+        execution_record: JobExecutionRecord | None,
+        environment_state: EnvironmentState,
+    ) -> Either[Schedule, str]:
+        if not environment.hosts:
+            return Either.error("no hosts given")
+        return self.f(job_instance, environment, execution_record, environment_state)
