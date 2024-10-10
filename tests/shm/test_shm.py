2e4c84aab239d6a4fdf4c36edfc9994cefa8fe51
Vojta Tuma <vojta.tuma@gmail.com>
2024-10-10 17:21:30 +0200

    [shm] basic implementation
    [shm] threadpools
    [shm] server more robust
    [shm] recovery from stale create/read
    [shm] extend api for executor integration

---
+++ tests/shm/test_shm.py
@@ -0,0 +1,74 @@
+import re
+from multiprocessing import Process
+
+import pytest
+
+import cascade.shm.api as api
+import cascade.shm.client as client
+import cascade.shm.server as server
+
+
+def test_shm_simple():
+    port = 12345
+    api.publish_client_port(port)
+    serverP = Process(target=server.entrypoint, args=(port,))
+    serverP.start()
+    try:
+        client.ensure()
+
+        buf = client.allocate("my_data", 7)
+        buf.view()[:] = b"a" * 7
+        buf.close()
+
+        with pytest.raises(ValueError, match=r"shm already closed"):
+            buf.view()
+
+        with pytest.raises(ValueError, match=re.escape("KeyError('missing_data')")):
+            client.get("missing_data")
+
+        buf = client.get("my_data")
+        assert buf.view() == b"a" * 7
+        with pytest.raises(TypeError, match="cannot modify read-only memory"):
+            buf.view()[:] = b"b" * 7
+        buf.close()
+
+    finally:
+        # test unclean exit:
+        serverP.terminate()
+
+
+def test_shm_disk():
+    port = 12346
+    capacity = 4
+    api.publish_client_port(port)
+    serverP = Process(target=server.entrypoint, args=(port, capacity))
+    serverP.start()
+    try:
+        client.ensure()
+
+        buf = client.allocate("m1", 3)
+        buf.view()[:] = b"a" * 3
+        buf.close()
+
+        assert client.get_free_space() == 1
+
+        with pytest.raises(ValueError, match="capacity exceeded"):
+            client.allocate("m2", 5)
+
+        # free space = 1 => allocate 2 first causes persist of m1
+        # thus free space will be first 4, then after allocate 2
+        buf = client.allocate("m2", 2)
+        buf.view()[:] = b"a" * 2
+        buf.close()
+
+        assert client.get_free_space() == 2
+
+        buf = client.get("m1")
+        assert buf.view() == b"a" * 3
+        # m1 was brought back up -> m2 persisted to disk, free space goes 2 -> 4 -> 1
+        assert client.get_free_space() == 1
+
+    finally:
+        # test clean exit:
+        client.shutdown()
+        serverP.join()
