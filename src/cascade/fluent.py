1de2a2b1dfd3e4f97ef2f8049366734fc5ac4bf2
Jenny Wong <jenny.wong@ecmwf.int>
2024-10-16 13:48:41 +0000

    Use node names in deduplication criteria

--- src/cascade/fluent.py
+++ src/cascade/fluent.py
@@ -150,7 +150,8 @@ class Action:
         self.nodes = nodes
 
     def graph(self) -> Graph:
-        """Creates graph from the nodes of the action.
+        """Creates graph from the nodes of the action, removing 
+        duplicate nodes. Assumes node names are unique.
 
         Return
         ------
@@ -161,7 +162,9 @@ class Action:
         for index in range(len(sinks)):
             sinks[index] = sinks[index].copy()
             sinks[index].outputs = []  # Ensures they are recognised as sinks
-        return deduplicate_nodes(Graph(sinks))
+        
+        pred = lambda a, b: (a.name == b.name and a.payload == b.payload)
+        return deduplicate_nodes(Graph(sinks), pred)
 
     @classmethod
     def register(cls, name: str, obj: type[Action]):
