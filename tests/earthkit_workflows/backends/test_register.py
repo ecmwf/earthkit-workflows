71d47cd00270022cc10e86af8166c7ed0efe63ff
Harrison Cook <harrison.cook@ecmwf.int>
2025-07-28 13:32:34 +0000

    fix tests

--- tests/earthkit_workflows/backends/test_register.py
+++ tests/earthkit_workflows/backends/test_register.py
@@ -6,6 +6,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+import pytest
+
 from earthkit.workflows import backends
 from earthkit.workflows.backends.arrayapi import ArrayAPIBackend
 
@@ -22,18 +24,41 @@ class Special(str):
     pass
 
 
-def test_single_register():
+@pytest.fixture
+def empty_backend():
+    """Fixture to reset the BACKENDS dictionary before each test."""
+    BACKEND_COPY = backends.BACKENDS.copy()
+    backends.BACKENDS.clear()
+    backends.BACKENDS.update(
+        {
+            object: ArrayAPIBackend,
+        }
+    )
+    yield
+    backends.BACKENDS.clear()
+    backends.BACKENDS.update(BACKEND_COPY)
+
+
+def test_single_register(empty_backend):
     backends.register(str, CustomStrBackend)
     backends.register(int, CustomStrBackend)
+
     assert backends.array_module("test") == CustomStrBackend
     assert backends.array_module(0) == CustomStrBackend
     assert backends.array_module(0.0) == ArrayAPIBackend
 
 
-def test_nested_register():
+def test_nested_register(empty_backend):
     backends.register(str, CustomStrBackend)
     backends.register(Special, CustomIntBackend)
 
     assert backends.array_module("test") == CustomStrBackend
     assert backends.array_module(Special("test")) == CustomIntBackend
     assert backends.array_module(0) == ArrayAPIBackend
+
+
+def test_lookup_register(empty_backend):
+    backends.register(str, CustomStrBackend)
+
+    assert backends.array_module("test") == CustomStrBackend
+    assert backends.array_module(Special("test")) == CustomStrBackend
