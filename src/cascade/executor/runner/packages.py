d9d0a0f358bb3cc8c4c02d853e77c3b291283237
Vojta Tuma <vojta.tuma@gmail.com>
2026-02-05 12:47:47 +0100

    [cascade/executor] improve package reinstalls diag
    when a module has already been loaded, and a different version
    of it gets installed, the worker's runner would be in a bad
    state -- the imported module does not change unda da hooda,
    but the task believes it did
    
    therefore, as a fuse breaking, we detect such situation post-hoc
    and fail rather than proceeding to the inevitable pickle failure
    or worse

--- src/cascade/executor/runner/packages.py
+++ src/cascade/executor/runner/packages.py
@@ -10,9 +10,12 @@
 
 Note that venv itself is left untouched after the run finishes -- we extend sys path
 with a temporary directory and install in there
+
+When extending, we check that none of the actually installed packages was not imported
+already with a different version
 """
 
-import importlib
+import importlib.metadata
 import logging
 import os
 import site
@@ -20,7 +23,10 @@ import subprocess
 import sys
 import tempfile
 from contextlib import AbstractContextManager
+from dataclasses import dataclass
 from typing import Literal
+
+from packaging.version import Version
 
 logger = logging.getLogger(__name__)
 
@@ -38,16 +44,16 @@ class Commands:
     ]
 
 
-def run_command(command: list[str]) -> None:
+def run_command(command: list[str]) -> tuple[str, str]:
     try:
-        result = subprocess.run(command, check=False, capture_output=True)
+        result = subprocess.run(command, check=False, capture_output=True, text=True)
     except FileNotFoundError as ex:
         raise ValueError(f"command failure: {ex}")
     if result.returncode != 0:
         msg = f"command failed with {result.returncode}. Stderr: {result.stderr}, Stdout: {result.stdout}, Args: {result.args}"
         logger.error(msg)
         raise ValueError(msg)
-
+    return result.stdout, result.stderr
 
 def new_venv() -> tempfile.TemporaryDirectory:
     """1. Creates a new temporary directory with a venv inside.
@@ -69,6 +75,75 @@ def new_venv() -> tempfile.TemporaryDirectory:
 
     return td
 
+def _parse_pip_install(pip_output: str) -> dict[str, Version]:
+    """Assumed input like: 'Using Python 3.11.8 environment at: <venv>\nResolved 1 package in 5ms\nUninstalled 1 package in 12ms\nInstalled 1 package in 18ms\n - numpy==2.4.2\n + numpy==2.4.1\n'
+    Provided output: {'numpy': '2.4.1'}"""
+    rv = {}
+    for line in pip_output.splitlines():
+        clean_line = line.strip()
+        if not clean_line.startswith("+"):
+            continue
+        
+        parts = clean_line.lstrip("+ ").split("==")
+        if len(parts) != 2:
+            logger.warning(f"Suspicious pip output: {clean_line} -- ignoring!")
+            continue
+        try:
+            rv[parts[0]] = Version(parts[1])
+        except Exception as e:
+            logger.warning(f"failed to parse package {parts[0]} version {parts[1]} due to {repr(e)}-- ignoring!")
+    return rv
+
+def _get_dist_modules(dist_name: str) -> list[str]:
+    # TODO presumably cacheable
+    try:
+        handle = importlib.metadata.distribution(dist_name)
+        top_level = handle.read_text('top_level.txt')
+        if top_level:
+            return top_level.split()
+        elif handle.files:
+            return list({path.parts[0] for path in handle.files if path.suffix == '.py'})
+        else:
+            logger.warning(f"neither files nor top level for {dist_name} -- ignoring")
+            return []
+    except Exception as e:
+        logging.warning(f"Could not find metadata for installed package: {dist_name} due to {repr(e)} -- ignoring")
+        return []
+
+def _maybe_module_version(mod_name: str) -> Version|None:
+    # TODO presumably cacheable, unless None
+    if mod_name in sys.modules:
+        mod = sys.modules[mod_name]
+        if hasattr(mod, "__version__"):
+            try:
+                return Version(mod.__version__)
+            except Exception as e:
+                logger.warning(f"failed to parse module {mod_name} version {mod.__version__} due to {repr(e)}-- ignoring!")
+        else:
+            logging.warning(f"Module '{mod_name}' is loaded, but has no __version__ attribute -- ignoring")
+
+@dataclass
+class InstallIssue:
+    dist_name: str
+    desired_version: Version
+    mod_issues: list[tuple[str, Version]]
+
+def _postinstall_verify(pip_output) -> list[InstallIssue]:
+    installed_packages = _parse_pip_install(pip_output)
+    rv = []
+
+    for dist_name, desired_version in installed_packages.items():
+        modules = _get_dist_modules(dist_name)
+        versions = [(m, _maybe_module_version(m)) for m in modules]
+        # NOTE eg torch is a bit of an issue here:
+        # pip reports + torch==2.7.1, but torch module version declares 2.7.1+cu126
+        # we have no real means of deciding whether this is correct or not -- but we make the assumption
+        # that checkpoints et cetera won't ever depend on this particular build discriminator => .base_version
+        mod_issues = [(m, v) for m, v in versions if v and v.base_version != desired_version.base_version] 
+        if mod_issues:
+            rv.append(InstallIssue(dist_name=dist_name, desired_version=desired_version, mod_issues=mod_issues))
+
+    return rv
 
 class PackagesEnv(AbstractContextManager):
     def __init__(self) -> None:
@@ -81,7 +156,7 @@ class PackagesEnv(AbstractContextManager):
             self.td = new_venv()
 
         logger.debug(
-            f"installing {len(packages)} packages: {','.join(packages[:3])}{',...' if len(packages) > 3 else ''}"
+            f"installing {len(packages)} packages: {','.join(packages[:10])}{',...' if len(packages) > 10 else ''}"
         )
         install_command = Commands.install_command(self.td.name)
         if os.environ.get("VENV_OFFLINE", "") == "YES":
@@ -90,15 +165,15 @@ class PackagesEnv(AbstractContextManager):
             install_command += ["--cache-dir", cache_dir]
         install_command.extend(set(packages))
         logger.debug(f"running install command: {' '.join(install_command)}")
-        run_command(install_command)
+        _, install_output = run_command(install_command)
 
-        # NOTE we need this due to namespace packages:
-        # 1. task 1 installs ns.pkg1 in its venv
-        # 2. task 1 finishes, task 2 starts on the same worker
-        # 3. task 2 starts, installs ns.pkg2. However, importlib is in a state that ns is aware only of pkg1 submod
-        # Additionally, the caches are invalid anyway, because task 1's venv is already deleted
-        importlib.invalidate_caches()
-        # TODO some namespace packages may require a reimport because they dynamically build `__all__` -- eg earthkit
+        # NOTE this is wrong because we would need to reliably unload modules, but that apparently aint possible in python
+        # importlib.invalidate_caches()
+        install_issues = _postinstall_verify(install_output)
+        if install_issues:
+            msg = repr(install_issues)
+            # TODO raise an error that would be converted to a message which then makes executor re-run the whole task sequence
+            raise ValueError(f"failed to install correctly: {msg[:1024]}{'...' if len(msg) > 1024 else ''}")
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> Literal[False]:
         sys.path = [
--- tests/cascade/executor/test_packages.py
+++ tests/cascade/executor/test_packages.py
@@ -2,7 +2,8 @@ import re
 
 import pytest
 
-from cascade.executor.runner.packages import run_command
+from cascade.executor.runner.packages import run_command, _parse_pip_install, _get_dist_modules, _maybe_module_version, _postinstall_verify, InstallIssue
+from packaging.version import Version
 
 
 def test_run_command() -> None:
@@ -22,3 +23,38 @@ def test_run_command() -> None:
         match=r"nonexistentpackagename was not found in the package registry",
     ):
         run_command(bad2_command)
+
+def test_parse_pip_install() -> None:
+    assumed = 'Using Python 3.11.8 environment at: <venv>\nResolved 1 package in 5ms\nUninstalled 1 package in 12ms\nInstalled 1 package in 18ms\n - numpy==2.4.2\n + numpy==2.4.1\n'
+    expected = {'numpy': Version('2.4.1')}
+    assert _parse_pip_install(assumed) == expected, "failed to parse assumed uv output"
+    import numpy
+    _, actual = run_command(["uv", "pip", "install", f"numpy=={numpy.__version__}"])
+    expected = {}
+    assert _parse_pip_install(actual) == expected, "failed to parse actual uv output"
+    # TODO it would be nice to actually do some pip install here, but im reluctant to do that in a unit test. Some for test_postinstall_verify
+
+def test_get_dist_modules() -> None:
+    assert _get_dist_modules("numpy") == ["numpy"]
+    assert sorted(_get_dist_modules("earthkit-workflows")) == sorted(["cascade", "earthkit"])
+
+def test_maybe_module_version() -> None:
+    import numpy
+    assert _maybe_module_version("numpy") == Version(numpy.__version__)
+    import cascade
+    assert _maybe_module_version("cascade") == None # whoopsie, we dont declare __version__ on cascade
+    import earthkit.workflows
+    if not hasattr(earthkit.workflows, "__version__"):
+        assert False # just to satisfy ty
+    assert _maybe_module_version("earthkit.workflows") == Version(earthkit.workflows.__version__)
+
+def test_postinstall_verify() -> None:
+    import numpy
+    goodie = 'Using Python 3.11.8 environment at: <venv>\nResolved 1 package in 5ms\nUninstalled 1 package in 12ms\nInstalled 1 package in 18ms\n - numpy==2.4.2\n + numpy=={numpy.__version__}\n'
+    issues = _postinstall_verify(goodie)
+    assert not issues
+    baddie = 'Using Python 3.11.8 environment at: <venv>\nResolved 1 package in 5ms\nUninstalled 1 package in 12ms\nInstalled 1 package in 18ms\n - numpy==2.4.2\n + numpy==1.0.0\n'
+    issues = _postinstall_verify(baddie)
+    assert issues == [
+        InstallIssue(dist_name='numpy', desired_version=Version('1.0.0'), mod_issues=[('numpy', Version(numpy.__version__))])
+    ]
