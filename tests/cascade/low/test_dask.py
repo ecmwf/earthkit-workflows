1852d8261076d20fee38ae44e11539b82e0e3ac5
Jenny Wong <jenny.wong@ecmwf.int>
2026-04-22 10:48:54 +0000

    Extract ext_outputs

--- tests/cascade/low/test_dask.py
+++ tests/cascade/low/test_dask.py
@@ -1,6 +1,6 @@
 from dask._task_spec import DataNode, Task, TaskRef
 
-from cascade.low.core import JobInstanceRich
+from cascade.low.core import DatasetId, DefaultTaskOutput, JobInstanceRich
 from cascade.low.dask import graph2job
 from cascade.main import run_locally
 
@@ -13,8 +13,8 @@ def test_dask():
     dask_graph = {"x": (x := DataNode(None, 1)), "y": (y := DataNode(None, 2)), "z": Task("z", add, TaskRef("x"), TaskRef("y"))}
     cascade_job = graph2job(dask_graph)
     outputs = run_locally(
-        JobInstanceRich(jobInstance=cascade_job, checkpointSpec=None, ext_outputs=list(cascade_job.outputs_of("'z'"))),
+        JobInstanceRich(jobInstance=cascade_job, checkpointSpec=None),
         workers=2,
         hosts=1,
     )
-    assert len(outputs) == 1
+    assert outputs == {DatasetId("'z'", DefaultTaskOutput): 3}
